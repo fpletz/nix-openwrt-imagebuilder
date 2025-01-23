969430d5abf1cab688094fa3553886dd7ee00c30
Franz Pletz <fpletz@fnordicwalking.de>
2025-01-23 06:15:38 +0100

    OpenWrt 24.10.0-rc6

--- releases.nix
+++ releases.nix
@@ -7,7 +7,7 @@ in {
   # Only OpenWrt >= 19.07.4 contains profiles.json files
   releases =
     builtins.filter (release:
-      builtins.compareVersions release "19.07.4" >= 0
+      builtins.compareVersions release "24" >= 0
     ) (
       map (builtins.replaceStrings [ ".nix" ] [ "" ]) (
         builtins.filter (lib.hasSuffix ".nix") (
